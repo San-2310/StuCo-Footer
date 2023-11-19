a41af091f6f6e0352a2fc605adc7af4ccde7a410
Sanjeev Ratnani <121822040+San-2310@users.noreply.github.com>
2023-11-19 19:09:38 +0530

    Update footer.jsx

--- footer.jsx
+++ footer.jsx
@@ -5,7 +5,6 @@
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
   />
-  {/* Bootstrap */}
   <link
     rel="stylesheet"
     href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
