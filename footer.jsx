49fc052b386ca2d6aa24b138a23f76916226a6c5
Sanjeev Ratnani <121822040+San-2310@users.noreply.github.com>
2023-11-19 19:08:20 +0530

    Add files via upload

---
+++ footer.jsx
@@ -0,0 +1,121 @@
+<>
+  <meta charSet="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+  />
+  {/* Bootstrap */}
+  <link
+    rel="stylesheet"
+    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+  />
+  <title>Footer</title>
+  <link rel="stylesheet" href="footer.css" />
+  <footer className="footer-section">
+    <div className="container">
+      <div className="footer-cta pt-5 pb-5">
+        <div className="row">
+          <div className="col-xl-4 col-md-4 mb-30">
+            <div className="single-cta phone1-cta">
+              <i className="fa fa-phone" />
+              <div className="cta-text">
+                <h4>Anmol Khy</h4>
+                <span>+91 80823 14934</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-4 col-md-4 mb-30">
+            <div className="single-cta mail-cta">
+              <i className="fa fa-envelope-open" />
+              <div className="cta-text">
+                <h4>Mail us</h4>
+                <span>student_council@spit.ac.in</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-4 col-md-4 mb-30">
+            <div className="single-cta phone2-cta">
+              <i className="fa fa-phone" />
+              <div className="cta-text">
+                <h4>Ankit Mishra</h4>
+                <span>+91 77382 28239</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer-content pt-5 pb-5">
+        <div className="row">
+          <div className="col-xl-6 col-lg-6 mb-50">
+            <div className="footer-widget">
+              <div className="footer-logo">
+                <a href="index.html">
+                  <img className="logo-img" src="images/stuco.png" alt="logo" />
+                </a>
+              </div>
+              <div className="footer-text">
+                <p>
+                  Empowering SPIT students, fostering community, encouraging
+                  collaboration, shaping a dynamic spirit.
+                </p>
+              </div>
+              <div className="footer-social-icon">
+                <span>Follow us</span>
+                {/* <a href="https://facebook.com/"><i class="fa fa-facebook-f facebook-bg"></i></a> */}
+                <a href="https://www.linkedin.com/school/bhartiya-vidya-bhavans-sardar-patel-institute-of-technology-munshi-nagar-andheri-mumbai/">
+                  <i className="fa fa-linkedin linkedin-bg" />
+                </a>
+                {/* <a href="https://google.com/"><i class="fa fa-google-plus google-bg"></i></a> */}
+                <a href="https://www.instagram.com/student_council_spit/">
+                  <i className="fa fa-instagram insta-bg" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-6 col-lg-6 col-md-6 mb-30">
+            <div className="footer-widget">
+              <div className="footer-widget-heading">
+                <h3>Useful Links</h3>
+              </div>
+              <ul className="useful-link">
+                <li>
+                  <a href="home.com">Home</a>
+                </li>
+                <li>
+                  <a href="events.com">Events</a>
+                </li>
+                <li>
+                  <a href="commit.com">Committees</a>
+                </li>
+                <li>
+                  <a href="team.com">Teams</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <article id="wrap">
+            <article id="lightings">
+              <section id="one">
+                <section id="two">
+                  <section id="three">
+                    <section id="four">
+                      <section id="five"></section>
+                    </section>
+                  </section>
+                </section>
+              </section>
+            </article>
+          </article>
+        </div>
+      </div>
+    </div>
+    <div className="copyright-area">
+      <div className="container">
+        <div className="copyright-text">
+          <p>Copyright © 2018, All Right Reserved Mithila</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</>
